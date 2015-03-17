d5c574351429896d6153cb5287a0d71e760e8b02
unknown <davidqtran12@gmail.com>
2015-03-16 23:05:56 -0500

    Separate JavaScript/jQuery 3/16/15

--- js/_main.js
+++ js/_main.js
@@ -36,20 +36,7 @@ $(document).ready(function() {
                              
 });
 
-jQuery(document).ready(function() {
-    jQuery('.tabs .tab-links a').on('click', function(e)  {
-        var currentAttrValue = jQuery(this).attr('href');
- 
-        // Show/Hide Tabs
-        jQuery('.tabs ' + currentAttrValue).siblings().slideUp(400);
-        jQuery('.tabs ' + currentAttrValue).delay(400).slideDown(400);
- 
-        // Change/remove current tab to active
-        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
- 
-        e.preventDefault();
-    });
-});
+
 
 
 //toggler
